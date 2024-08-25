4d00994501f7e41873f6508411f2ce471a62b8fb
HafidzKholifatur <hafidz.kholifatur@gmail.com>
2024-08-25 23:30:34 +0700

    Func Text Editor Context & Changing Editor State | BlogEditor Comp, Editor Pages | issue #36

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -11,16 +11,16 @@ import { tools } from "./tools.component";
 
 const BlogEditor = () => {
 
-    let { blog, blog: { title, banner, content, tags, des }, setBlog } = useContext(EditorContext)
+    let { blog, blog: { title, banner, content, tags, des }, setBlog, textEditor, setTextEditor, setEditorState } = useContext(EditorContext)
 
     // useEffect
     useEffect(() => {
-        let editor = new EditorJS({
+        setTextEditor(new EditorJS({
             holderId: "textEditor",
             data: '',
             tools: tools,
             placeholder: "Lets write an awesome story"
-        })
+        }))
     }, [])
 
     const handleBannerUpload = (e) => {
@@ -69,6 +69,31 @@ const BlogEditor = () => {
         img.src = defaultBanner;
     }
 
+    const handlePublishEvent = () => {
+
+        if(!banner.length){
+            return toast.error("Upload a blog banner to publish it.")
+        }
+
+        if(!title.length){
+            return toast.error("Write blog title to publish it.")
+        }
+
+        if(textEditor.isReady){
+            textEditor.save().then(data => {
+                if(data.blocks.length){
+                    setBlog({ ...blog, content: data });
+                    setEditorState("publish");
+                } else{
+                    return toast.error("Write something in your blog to publish it.")
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        }
+    }
+
     return (
         <>
             <nav className="navbar">
@@ -80,7 +105,7 @@ const BlogEditor = () => {
                 </p>
 
                 <div className="flex gap-4 ml-auto">
-                    <button className="btn-dark py-2">
+                    <button className="btn-dark py-2" onClick={handlePublishEvent}>
                         Publish
                     </button>
                     <button className="btn-light py-2">
